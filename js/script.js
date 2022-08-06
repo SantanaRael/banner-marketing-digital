89c2dc5e1e386aad7d2dd28811af28e607953970
Israel Santana <73674173+SantanaRael@users.noreply.github.com>
2022-08-05 22:14:58 -0300

    foi adicionado todos os arquivos ao repositório

---
+++ js/script.js
@@ -0,0 +1,15 @@
+ScrollReveal({
+    reset: true,
+    distance:'60px',
+    duration: 2500,
+    delay:400
+})
+
+ScrollReveal().reveal('.logo', {delay: 700, origin: 'left'});
+ScrollReveal().reveal('main', {delay: 800, origin: 'left'});
+ScrollReveal().reveal('.site', {delay: 700, origin: 'left'});
+
+
+ScrollReveal().reveal('.contact', {delay: 700, origin: 'right'});
+ScrollReveal().reveal('.galery', {delay: 700, origin: 'right'});
+ScrollReveal().reveal('.controllers', {delay: 700, origin: 'right'});
